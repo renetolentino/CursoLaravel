cd02b174939ded1f65065759a5ea785090304ff8
Renê Tolentino <114789737+renetolentino@users.noreply.github.com>
2022-10-23 16:36:59 -0300

    Migration
    Stablished a Database and created some models and migrations, set up the env file and did some tests

--- database/migrations/2022_10_23_172852_create_fornecedors_table.php
+++ database/migrations/2022_10_23_172852_create_fornecedors_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nome', 50);
-            $table->string('telefone',20);
-            $table->string('email',80);
-            $table->integer('motivo_contato');
-            $table->text('mensagem');
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('site_contatos');
+        Schema::dropIfExists('fornecedores');
     }
 };
--- database/migrations/2022_10_23_183201_alter_fornecedores_novas_colunas.php
+++ database/migrations/2022_10_23_183201_alter_fornecedores_novas_colunas.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('nome', 50);
-            $table->string('telefone',20);
-            $table->string('email',80);
-            $table->integer('motivo_contato');
-            $table->text('mensagem');
+        //
+        Schema::table('fornecedores', function(Blueprint $table){
+            $table->string('UF', 2);
+            $table->string('email', 150);
         });
     }
 
@@ -31,6 +27,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('site_contatos');
+        //
+        Schema::table('fornecedores', function(Blueprint $table) {
+            $table->dropColumn(['UF', 'email']);
+        });
     }
 };
